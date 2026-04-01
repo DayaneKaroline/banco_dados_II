09ba4031c09d01ffb91dd8794745a287cfc48930
DayaneKaroline <dayanefalves33@gmail.com>
2026-04-01 20:25:31 -0300

    at
    atualização do SELECT

--- SQL/SQLQuery1inserindodados-select.sql
+++ SQL/SQLQuery1inserindodados-select.sql
@@ -11,3 +11,22 @@ select nome as 'nome alunos', cpf from alunos_dayanek
 select *from turmas_dayanek where turno = 'Noite'
 
 select * from alunos_dayanek where id =2
+
+select * from alunos_dayanek where turma_id = 1
+
+select * from disciplinas_dayanek where professor_id = 1
+
+select *from alunos_dayanek where turma_id = 1 and id < 3
+
+select * from turmas_dayanek where turno in ('Manhã','Tarde')
+
+select * from professores_dayanek where nome LIKE 'Ana%'
+
+select *from alunos_dayanek order by nome asc
+
+select *from alunos_dayanek order by nome desc
+
+select id, nome from disciplinas_dayanek order by nome asc
+
+select *from turmas_dayanek where turno in ('Manhã','Noite') 
+order by nome asc
