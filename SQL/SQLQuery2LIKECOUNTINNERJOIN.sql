29469430720b6f435c2499e83f27a4e3e0f3f84a
DayaneKaroline <dayanefalves33@gmail.com>
2026-04-01 20:40:38 -0300

    AT
    LIKE,COUNT,INNERJOIN

---
+++ SQL/SQLQuery2LIKECOUNTINNERJOIN.sql
@@ -0,0 +1,9 @@
+select *from professores_dayanek where nome LIKE 'A%'
+
+select *from alunos_dayanek where nome LIKE '%a'
+
+select *from disciplinas_dayanek where nome LIKE '%Mobile%'
+
+select *from professores_dayanek where email LIKE '%@gmail.com'
+
+select *from alunos_dayanek
