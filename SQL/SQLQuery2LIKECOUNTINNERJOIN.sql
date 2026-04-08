f04ba55fd7b7b5af408924582ec6d232b0073a70
DayaneKaroline <dayanefalves33@gmail.com>
2026-04-08 20:41:16 -0300

    at
    continuação

--- SQL/SQLQuery2LIKECOUNTINNERJOIN.sql
+++ SQL/SQLQuery2LIKECOUNTINNERJOIN.sql
@@ -7,3 +7,21 @@ select *from disciplinas_dayanek where nome LIKE '%Mobile%'
 select *from professores_dayanek where email LIKE '%@gmail.com'
 
 select *from alunos_dayanek
+
+select COUNT(*) AS Total from alunos_dayanek
+
+select a.nome AS 'Nome do Aluno',
+       t.nome AS 'Nome da Turma' 
+from alunos_dayanek a 
+INNER JOIN turmas_dayanek t ON a.turma_id = t.id
+
+select a.nome AS 'Nome do Aluno'
+       ,t.nome AS 'Nome da Turma'
+       ,t.turno
+fROM alunos_dayanek a 
+INNER JOIN turmas_dayanek t ON a.turma_id = t.id
+
+select d.nome AS 'Nome das Disciplinas'
+      ,p.nome AS 'Nome do Professor'
+FROM disciplinas_dayanek d 
+INNER JOIN professores_dayanek p ON p.id = d.professor_id
