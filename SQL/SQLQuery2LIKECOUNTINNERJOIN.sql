8f5d42f816c8839f67a6c102b31abc5349dd7d9f
DayaneKaroline <dayanefalves33@gmail.com>
2026-04-15 20:41:36 -0300

    at
    atualização

--- SQL/SQLQuery2LIKECOUNTINNERJOIN.sql
+++ SQL/SQLQuery2LIKECOUNTINNERJOIN.sql
@@ -25,3 +25,22 @@ select d.nome AS 'Nome das Disciplinas'
       ,p.nome AS 'Nome do Professor'
 FROM disciplinas_dayanek d 
 INNER JOIN professores_dayanek p ON p.id = d.professor_id
+
+select d.nome AS 'Nome das Disciplinas'
+      ,p.email AS 'Email do Professores'
+ FROM disciplinas_dayanek d
+ INNER JOIN professores_dayanek p ON d.professor_id = p.id
+
+ select a.nome AS ' Aluno'
+       ,d.nome 'Disciplina'
+FROM alunos_dayanek a 
+INNER JOIN alunos_disciplinas_dayanek
+ad ON a.id = ad.alunos_id
+INNER JOIN  disciplinas_dayanek d ON d.id = 
+ad.disciplinas_id
+
+select a.* FROM alunos_dayanek a 
+INNER JOIN turmas_dayanek t ON t.id = a.turma_id
+WHERE t.turno = 'Manhã'
+
+select 
